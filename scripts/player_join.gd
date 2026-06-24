c408bc9d98391b3a7440766a72e10dd8aa419fb6
kLim-lab <kenanlimo41@gmail.com>
2026-06-24 10:10:48 -0400

    Connect signals for player join and leave

--- scripts/player_join.gd
+++ scripts/player_join.gd
@@ -5,19 +5,7 @@ var selected_avatar: String = ""
 func _on_join_pressed() -> void:
     var room_code = $VBoxContainer/HBoxContainer/LineEdit.text.strip_edges()
     var player_name = $VBoxContainer/HBoxContainer2/LineEdit.text.strip_edges()
-'''
-    if room_code == "":
-        print("Please enter a room code")
-        return
 
-    if player_name == "":
-        print("Please enter your name")
-        return
-
-    if selected_avatar == "":
-        print("Please select an avatar")
-        return
-'''
     print("Joining room: " + room_code + " as " + player_name + " with avatar " + selected_avatar)
 
 func _on_avatar_selected(emoji: String) -> void:
