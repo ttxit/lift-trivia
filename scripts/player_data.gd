ce6cc78417fd6f5053571859573628bacfc5eea6
kLim-lab <kenanlimo41@gmail.com>
2026-06-25 15:07:16 -0400

    Fix fit-to-screen for player join  and player waiting scene

--- scripts/player_data.gd
+++ scripts/player_data.gd
@@ -2,34 +2,34 @@ extends Node
 
 # accessible from anywhere in the project as "PlayerData"
 
-# var player_name: String = ""
-# var avatar: String = ""
-# var room_code: String = ""
-
-class Player:
-	var player_name: String = ""
-	var avatar: String = ""
-	var points: int = 0
-	
-	func _init(p_name: String, p_avatar: String) -> void:
-		player_name = p_name
-		avatar = p_avatar
-	
-	func add_points(amount: int) -> void:
-		points += amount
-	
-    # readable string summary of the player's current state
-	func to_string() -> String:
-		return player_name + " (" + avatar + "): " + str(points) + " pts"
-
-# player object for local device's player
-var current_player: Player = null
-
+var player_name: String = ""
+var avatar: String = ""
 var room_code: String = ""
 
+#class Player:
+#	var player_name: String = ""
+#	var avatar: String = ""
+#	var points: int = 0
+	
+#	func _init(p_name: String, p_avatar: String) -> void:
+#		player_name = p_name
+#		avatar = p_avatar
+	
+#	func add_points(amount: int) -> void:
+#		points += amount
+	
+	# readable string summary of the player's current state
+#	func to_string() -> String:
+#		return player_name + " (" + avatar + "): " + str(points) + " pts"
+
+# player object for local device's player
+#var current_player: Player = null
+
+#var room_code: String = ""
+
 # creates a new Player object and assigns it to current_player, called when player clicks "Join"
-func create_player(p_name: String, p_avatar: String) -> void:
-	current_player = Player.new(p_name, p_avatar)
+#func create_player(p_name: String, p_avatar: String) -> void:
+#	current_player = Player.new(p_name, p_avatar)
 
 # for leaderboard sorting, returns a new array of players sorted by points (highest first)
 #func get_sorted_players(players: Array) -> Array:
--- scripts/player_join.gd
+++ scripts/player_join.gd
@@ -6,7 +6,12 @@ func _on_join_pressed() -> void:
 	var room_code = $VBoxContainer/VBoxContainer/room_code/LineEdit.text.strip_edges()
 	var player_name = $VBoxContainer/VBoxContainer/player_name/LineEdit.text.strip_edges()
 
-	PlayerData.create_player(player_name, selected_avatar)
+	# for class implementation, replace lines 13-15 with:
+	# PlayerData.create_player(player_name, selected_avatar)
+	# PlayerData.room_code = room_code
+
+	PlayerData.player_name = player_name
+	PlayerData.avatar = selected_avatar
 	PlayerData.room_code = room_code
 
 	SceneManager.change_view("res://scenes/player_waiting.tscn")
