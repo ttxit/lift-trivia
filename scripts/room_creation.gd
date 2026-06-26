9a3679749973ff5b6f29c230d6d1c590aad1bc82
Temitayo Ibitayo <93548431+ttxit@users.noreply.github.com>
2026-06-26 14:00:16 -0400

    Connect room_creation to waiting_room

--- scripts/room_creation.gd
+++ scripts/room_creation.gd
@@ -3,3 +3,6 @@ extends Control
 
 func _on_back_pressed() -> void:
 	SceneManager.change_view("res://scenes/main_menu.tscn")
+
+func _on_start_pressed() -> void:
+	SceneManager.change_view("res://scenes/waiting_room.tscn")
