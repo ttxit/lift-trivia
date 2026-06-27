062b247c4073ddc22b8fd9400546e832f8a1cc3f
Temitayo Ibitayo <93548431+ttxit@users.noreply.github.com>
2026-06-27 00:02:44 -0400

    Add back and start buttons to waiting_room

---
+++ scripts/waiting_room.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_back_pressed() -> void:
+    SceneManager.change_view("res://scenes/room_creation.tscn")
+
+
+func _on_start_pressed() -> void:
+    pass # Replace with function body.
