835b28d4a1c0918efc74e5f59250d2f9aafc4e81
Temitayo Ibitayo <93548431+ttxit@users.noreply.github.com>
2026-06-27 00:05:51 -0400

    Change Start in room_creation to Create
    It seemed repetitive to have a player press "create" twice in succession, so I changed the name.

--- scripts/room_creation.gd
+++ scripts/room_creation.gd
@@ -4,5 +4,6 @@ extends Control
 func _on_back_pressed() -> void:
 	SceneManager.change_view("res://scenes/main_menu.tscn")
 
-func _on_start_pressed() -> void:
+
+func _on_create_pressed() -> void:
 	SceneManager.change_view("res://scenes/waiting_room.tscn")
