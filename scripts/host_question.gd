befdf9c49ba7b24dd7835f303cba0701c26db6f0
Temitayo Ibitayo <93548431+ttxit@users.noreply.github.com>
2026-07-13 19:28:52 -0400

    Convert indentation to tabs

--- scripts/host_question.gd
+++ scripts/host_question.gd
@@ -3,17 +3,17 @@ extends Control
 # initialize timer for countdown
 
 #func _ready() -> void:
-    # load question and answers from file to overwrite placeholder text on scene
+	# load question and answers from file to overwrite placeholder text on scene
 
-    #$VBoxContainer/top_row/number_bubble/question_number.text = "3"
-    #$VBoxContainer/top_row/timer_bubble/timer.text = "15"
-    #$VBoxContainer/question_bubble/question.text = "Which book of the Bible contains the Sermon on the Mount?"
-    #$VBoxContainer/answer_grid/a1_bubble/answer1.text.strip_edges() = "Matthew"
-    #$VBoxContainer/answer_grid/a2_bubble/answer2.text.strip_edges() = "Luke"
-    #$VBoxContainer/answer_grid/a3_bubble/answer3.text.strip_edges() = "Mark"
-    #$VBoxContainer/answer_grid/a4_bubble/answer4.text.strip_edges() = "John"
+	#$VBoxContainer/top_row/number_bubble/question_number.text = "3"
+	#$VBoxContainer/top_row/timer_bubble/timer.text = "15"
+	#$VBoxContainer/question_bubble/question.text = "Which book of the Bible contains the Sermon on the Mount?"
+	#$VBoxContainer/answer_grid/a1_bubble/answer1.text.strip_edges() = "Matthew"
+	#$VBoxContainer/answer_grid/a2_bubble/answer2.text.strip_edges() = "Luke"
+	#$VBoxContainer/answer_grid/a3_bubble/answer3.text.strip_edges() = "Mark"
+	#$VBoxContainer/answer_grid/a4_bubble/answer4.text.strip_edges() = "John"
 
 # functions to select answer and send to players for display
 
 func _on_end_pressed() -> void:
-    SceneManager.change_view("res://scenes/main_menu.tscn")
+	SceneManager.change_view("res://scenes/main_menu.tscn")
--- scripts/waiting_room.gd
+++ scripts/waiting_room.gd
@@ -2,8 +2,8 @@ extends Control
 
 
 func _on_back_pressed() -> void:
-    SceneManager.change_view("res://scenes/room_creation.tscn")
+	SceneManager.change_view("res://scenes/room_creation.tscn")
 
 
 func _on_start_pressed() -> void:
-    pass # Replace with function body.
+	pass # Replace with function body.
