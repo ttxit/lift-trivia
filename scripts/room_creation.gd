52bc9b7fe4451c832b0e922f7d5d1603a5b895b3
Temitayo Ibitayo <93548431+ttxit@users.noreply.github.com>
2026-08-07 15:02:29 -0400

    Add category selection logic
    Add placeholder logic for selecting categories at room creation.
    
    For now, the category is replaced with the sample category until other category files are created.

--- scripts/room_creation.gd
+++ scripts/room_creation.gd
@@ -1,12 +1,30 @@
 extends Control
 
 
+## Category array based on order of options in the OptionButton node
+const CATEGORY_ARRAY: Array[String] = [
+	"bible",
+	"church_members",
+	"lift_lore",
+	"campus"
+]
+
+@onready var category_selection: OptionButton = $"VBoxContainer/options/room_settings/HBoxContainer/category"
+
+
 func _on_back_pressed() -> void:
 	SceneManager.change_view("res://scenes/main_menu.tscn")
 
 
 func _on_create_pressed() -> void:
+	if category_selection.selected == -1:
+		print("Please select a category for questions")
+
+	var selected_string: String = CATEGORY_ARRAY[category_selection.selected]
+
 	var question_count: int = $"VBoxContainer/options/room_settings/questions_count/SpinBox".value
-	QuestionManager.save_question_sequence(question_count, "sample_category")
+
+	selected_string = "sample_category" # Remove this line when other category files exist
+	QuestionManager.save_question_sequence(question_count, selected_string)
 
 	SceneManager.change_view("res://scenes/waiting_room.tscn")
